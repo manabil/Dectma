affb23a7687416a8ca734200bacee8e6adec18d5
manabil <mammarnabil@students.unnes.ac.id>
2022-12-05 10:52:20 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn btn-primary">Klik Disini</button>
+    </div>
+  );
+}
+
+export default App;
